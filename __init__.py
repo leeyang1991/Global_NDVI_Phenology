8a006e87d7ebcf32f84802326a63985a3ab94436
Yang Li <leeyang1991@gmail.com>
2020-04-08 12:44:32 +0800

    initialize params

---
+++ __init__.py
@@ -0,0 +1,33 @@
+# coding='gbk'
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy import interpolate
+from scipy import signal
+import time
+import to_raster
+import ogr, os, osr
+from tqdm import tqdm
+import datetime
+from scipy import stats, linalg
+import pandas as pd
+import seaborn as sns
+from matplotlib.font_manager import FontProperties
+import imageio
+from scipy.stats import gaussian_kde as kde
+import matplotlib as mpl
+import multiprocessing
+from multiprocessing.pool import ThreadPool as TPool
+import copy_reg
+import types
+from scipy.stats import gamma as gam
+import math
+import copy
+import scipy
+import sklearn
+import ternary
+import random
+import h5py
+from LY_Tools import *
+this_root = 'D:\\project_phenology\\'
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
